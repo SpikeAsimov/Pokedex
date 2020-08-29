c8885dd0b7080f465896f9594985382aabb4ebe8
ikrak <hualterio@gmail.com>
2020-08-29 04:45:21 -0300

    agrega contenido en App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,94 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+
+    <Header/>
+
+
+    <section class="wrapper">
+      <div class="content">
+        <section class="description-container">
+          <div class="description">
+              <Description :pokemon="pokemon"/>
+          </div>
+          <div class="picture">
+              <ImagePokemon :img-url="pokemon.sprites.front_shiny"/>
+          </div>
+        </section>
+      </div>
+    </section>
+
+
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Header from './components/Header.vue'
+import Description from './components/Description.vue'
+import ImagePokemon from "./components/ImagePokemon";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      Header,
+      Description,
+      ImagePokemon
+  },
+  data: () => {
+    return {
+      pokemon: {}
+    }
+  },
+  created() {
+    console.log("Created...")
+
+    fetch("https://pokeapi.co/api/v2/pokemon/6/")
+    .then(response => response.json())
+    .then(data => (this.pokemon = data))
+    .catch(error => console.error(error));
+
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
+  .content {
+    width: 60%;
+    border: 5px hsl(353, 91%, 12%) solid;
+    border-radius: 16px;
+    padding: 18px;
+    background: hsl(353, 81%, 61%);
+    max-width: 850px;
+    margin-top: 7%;
+    margin-bottom: 22px;
+  }
+
+  .description-container {
+    display: flex;
+    flex-wrap: wrap-reverse;
+  }
+
+  .description {
+    flex: 1;
+
+  }
+
+  .picture {
+    flex: 1;
+
+  }
+
+  .wrapper {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+  }
+
+  main {
+    min-width: 100vw;
+    min-height: 100vh;
+    background: hsl(353, 90%, 90%);
+  }
+
 </style>
