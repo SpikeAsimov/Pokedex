e1745756075f87a249e8ce0128e14f990d4ab585
Spike Asimov <hualterio@gmail.com>
2021-06-06 19:11:17 -0300

    agregar archivo para deploy

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'haciendo deploy en gh-pages..'
+
+# if you are deploying to https://<USERNAME>.github.io
+#git push -f git@github.com:spikeasimov/spikeasimov.github.io.git prod
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:SpikeAsimov/Pokedex.git master:prod
+
+cd -
